68f5f3bb8f9580f487578dc32477077c5d3f9f28
vivianedias <vivipirolad@gmail.com>
2018-11-14 22:32:32 -0200

    novo npm init

---
+++ fullstack/node/aula-3/reprograma-curso-ferramentas/index.js
@@ -0,0 +1,7 @@
+const soma = (a, b) => a + b;
+const digaOla = mensagem => {
+    console.log(mensagem);
+}
+
+module.exports.digaOla = digOla;
+module.exports.soma = soma;
---
+++ fullstack/node/aula-3/require-loadash.js
@@ -0,0 +1,5 @@
+// Usando um módulo a partir do NPM
+const _ = require('loadash');
+const arr = [1, 4, 6, 9];
+
+console.log(_.inclues(arr,9))
